8087cb9e36e52ad5a62049d7fc360e18ec2b045b
VGhazaryan <49144300+VGhazaryan@users.noreply.github.com>
2019-12-24 15:15:28 +0400

    Added checkDate function

--- string_util.py
+++ string_util.py
@@ -1,6 +1,16 @@
+import datetime
 content = "One morning, when Gregor Sam5a woke fr0m troubled dream5, he found h1mself transformed in hi5 bed into a h0rr1ble verm1n."
 
 
 def findNumbers(content):
     return [char for char in content if (char.isdigit()) == True]
 print(findNumbers(content))
+
+def checkDate(date):
+    isDate = True
+    day, month, year = date.split('/')
+    try:
+        datetime.datetime(int(year), int(month), int(day))
+    except ValueError:
+        isDate = False
+    return isDate
